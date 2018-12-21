64d07962dacd72de41bf16eea2044211ca3f0283
rajesh kumar <rajesh@thb.co.in>
2018-12-21 16:14:12 +0530

    now child is calling parent

--- child/child.go
+++ child/child.go
@@ -2,8 +2,10 @@ package child;
 
 import (
 "fmt"
+"github.com/rajeshpachar/hellomod"
 )
 
 func HelloChild(){
 fmt.Println("child is looking good ")
+hellotest.SayHello("child is calling parent")
 }
